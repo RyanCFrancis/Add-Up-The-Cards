a27843fc61b3b5e5d691832c7a4bfac929afcc7b
Ryan Francis <118226073+RyanCFrancis@users.noreply.github.com>
2022-12-11 21:21:31 -0500

    adapter fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,4 @@
-//old
-// import adapter from '@sveltejs/adapter-auto';
-// import { vitePreprocess } from '@sveltejs/kit/vite';
-
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-// 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-// 	// for more information about preprocessors
-// 	preprocess: vitePreprocess(),
-
-// 	kit: {
-// 		adapter: adapter()
-// 	}
-// };
-
-// export default config;
-
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -25,15 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false,
-			strict: true
-		})
+		adapter: adapter()
 	}
 };
 
