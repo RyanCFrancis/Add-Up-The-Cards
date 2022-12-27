3115b6da665c706ba9683d07246d4c89788a63be
Ryan Francis <118226073+RyanCFrancis@users.noreply.github.com>
2022-12-27 02:51:36 -0500

    failed promise

--- src/routes/Account/+page.svelte
+++ src/routes/Account/+page.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
 	import { supabase } from '$lib/supabaseClient';
 	import { onMount } from 'svelte';
+
+	async function toDo() {
+		const {
+			data: { user }
+		} = await supabase.auth.getUser();
+		return user;
+	}
+	onMount(() => {
+		toDo();
+	});
 </script>
+
+{#await toDo() then data}
+	testing{data}
+{/await}
 
 <style>
 </style>
