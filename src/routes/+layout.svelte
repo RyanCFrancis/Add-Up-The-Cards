5db52449d5c0e7ed8d7817264adf3ded21356f8d
Ryan Francis <118226073+RyanCFrancis@users.noreply.github.com>
2022-12-27 02:41:44 -0500

    added jwt parsing help??

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
 	import './styles.css';
+	import { onMount } from 'svelte';
+	import { invalidate } from '$app/navigation';
 	import { supabase } from '$lib/supabaseClient';
 	let innerWidth: number;
+
+	onMount(() => {
+		const {
+			data: { subscription }
+		} = supabase.auth.onAuthStateChange(() => {
+			invalidate('supabase:auth');
+		});
+
+		return () => {
+			subscription.unsubscribe();
+		};
+	});
 </script>
 
 <head />
@@ -14,7 +28,15 @@
 	<ul class="sidebar">
 		<a href="/" class="sidebarItem">Game</a>
 		<a href="/Login" class="sidebarItem">Login</a>
-		<a href="/Signout" class="sidebarItem">Sign Out</a>
+		<!-- svelte-ignore a11y-invalid-attribute -->
+		<a
+			href=""
+			class="sidebarItem"
+			on:click={() => {
+				supabase.auth.signOut();
+				alert('you have been signed out');
+			}}>Sign Out</a
+		>
 		<a href="/Account" class="sidebarItem">Your Account</a>
 		<a href="/Scores" class="sidebarItem">High Scores</a>
 	</ul>
--- src/routes/Login/+page.svelte
+++ src/routes/Login/+page.svelte
@@ -8,11 +8,10 @@
 		const { data, error } = await supabase.auth.signInWithOAuth({
 			provider: 'google',
 			options: {
-				//redirectTo: 'https://add.up.the.cards.vercel.app'
+				redirectTo: '/Scores'
 			}
 		});
 		const authToken = data;
-		console.log(authToken);
 	}
 
 	onMount(() => {
