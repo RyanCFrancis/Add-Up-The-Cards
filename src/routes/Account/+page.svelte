e67d0352272041bd5a749800777dc18cf3e75c21
Ryan Francis <118226073+RyanCFrancis@users.noreply.github.com>
2022-12-28 17:17:10 -0500

    added supabase boiler and got name to console.log

--- src/routes/Account/+page.svelte
+++ src/routes/Account/+page.svelte
@@ -1,15 +1,30 @@
 <script lang="ts">
 	import { supabase } from '$lib/supabaseClient';
 	import { onMount } from 'svelte';
-	let currentSession: any;
+	import type { AuthSession } from '@supabase/supabase-js';
+
+	export let currentSession: AuthSession;
+	let currentUser: any;
 
 	onMount(async () => {
-		currentSession = await supabase.auth.getSession();
-		console.log(currentSession.data.session.user.id);
+		//currentSession = await supabase.auth.getSession();
+		//console.log('google-id = ' + currentSession.data.session.user.id);
+
+		let { data: Users, error } = await supabase.from('Users').select('Name');
+
+		currentUser = Users;
+		console.log(currentUser[0].Name);
+		//console.log('test');
 	});
 </script>
 
-<div>{currentSession}</div>
+{#await supabase.auth.getSession()}
+	<p>waiting</p>
+{:then}
+	<p>{currentUser[0].Name}</p>
+{:catch error}
+	<p style="color: red">{error.message}</p>
+{/await}
 
 <style>
 </style>
