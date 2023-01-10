8842d41cad99d4e13f645cb89c110792bd8b77f1
Ryan Francis <118226073+RyanCFrancis@users.noreply.github.com>
2023-01-10 00:37:36 -0500

    css shenanigans

--- src/routes/Profile/[userid]/+page.svelte
+++ src/routes/Profile/[userid]/+page.svelte
@@ -2,6 +2,27 @@
 	console.log('test');
 	export let data: any;
 	console.log(data);
+
+	function timeToString(time: number) {
+		let seconds = 0;
+		let milliseconds = 0;
+		let minutes = 0;
+		if (time >= 120000) {
+			minutes = Math.floor(time / 60000);
+			seconds = Math.floor(time / 1000) - minutes * 60;
+			milliseconds = time % 1000;
+			return minutes + ' minutes and ' + seconds + '.' + milliseconds + ' seconds';
+		} else if (time >= 60000 && time < 120000) {
+			minutes = Math.floor(time / 60000);
+			seconds = Math.floor(time / 1000) - minutes * 60;
+			milliseconds = time % 1000;
+			return minutes + ' minute and ' + seconds + '.' + milliseconds + ' seconds';
+		} else {
+			seconds = Math.floor(time / 1000);
+			milliseconds = time % 1000;
+			return seconds + '.' + milliseconds + ' seconds';
+		}
+	}
 </script>
 
 <main>
@@ -20,4 +41,33 @@
 	>
 		{data.playerName}
 	</p>
+
+	<table>
+		<thead>
+			<th>Time</th>
+			<th>Combination</th>
+			<th>Date + Time Made</th>
+		</thead>
+		<tbody
+			>{#each data.scores as { score, combo_one, combo_two, combo_three, created_at }}
+				<tr
+					><td> {timeToString(score)}</td>
+					<td
+						>{combo_one}
+						{combo_two}
+						{combo_three}</td
+					>
+					<td>{new Date(created_at).toLocaleString()}</td>
+				</tr>
+			{/each}</tbody
+		>
+	</table>
 </main>
+
+<style>
+	main {
+		transform: translateX(-50%);
+		left: 50%;
+		position: absolute;
+	}
+</style>
